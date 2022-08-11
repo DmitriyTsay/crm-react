6ead2c3faca709753627be46a67a9c23a3036da2
DmitriyTsay <106547568+DmitriyTsay@users.noreply.github.com>
2022-08-12 01:51:32 +0600

    Ticket card component update

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,8 +1,24 @@
 
+import { Link } from 'react-router-dom'
+
+import AvatarDisplay from './AvatarDisplay'
+import StatusDisplay from './StatusDisplay'
+import PriorityDisplay from './PriorityDisplay'
+import ProgressDisplay from './ProgressDisplay'
+import DeleteBlock from './DeleteBlock'
+
 export default function TicketCard() {
     return (
         <div className="ticket-card">
-            TicketCard
+            <Link>
+                <div className="ticket-color"></div>
+                <h3>title</h3>
+                <AvatarDisplay />
+                <StatusDisplay />
+                <ProgressDisplay />
+                <PriorityDisplay />
+            </Link>
+            <DeleteBlock />
         </div>
     )
 }
