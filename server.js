66e4bafc43e527725fd5ecd560d4983e8f4b0e3b
DmitriyTsay <106547568+DmitriyTsay@users.noreply.github.com>
2022-08-23 17:52:18 +0600

    Added express static (for heroku)

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const app = express()
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './build')))
 
 const url = process.env.URL
 const token = process.env.ASTRA_TOKEN
